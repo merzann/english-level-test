69784a710b16601a5982979ca8b0ab47cabcc229
merzann <annisch78@gmail.com>
2024-08-08 04:45:12 +0000

    maint: update run.py to comply with PEP8 standards including line length

--- README.md
+++ README.md
@@ -1,32 +1,56 @@
-![CI logo](https://codeinstitute.s3.amazonaws.com/fullstack/ci_logo_small.png)
+# English Level Assessment Quiz
 
-Welcome,
 
-This is the Code Institute student template for deploying your third portfolio project, the Python command-line project. The last update to this file was: **May 14, 2024**
+## Overview
+This Python application assesses a student's English level based on vocabulary, grammar, and text comprehension. Results are recorded in Google Sheets, saved to a JSON file  and then exported to Zapier to automatically send an email to the student with their results.
+The application is part of a learning platform from a Homestay Host & English Tutor who uses the results for developping an individual course plan for each student to address their needs in the most efficient way.
 
-## Reminders
+## Features
+- Import questions from Google Sheets
+- Administer quiz via command-line interface
+- Calculate and determine CEFR level
+- Record and export results
 
-- Your code must be placed in the `run.py` file
-- Your dependencies must be placed in the `requirements.txt` file
-- Do not edit any of the other files or your code may not deploy properly
+## Validator Testing:
 
-## Creating the Heroku app
+    - PEP8
+    
 
-When you create the app, you will need to add two buildpacks from the _Settings_ tab. The ordering is as follows:
+## Deployment
+    
+This project was deployed using a mock terminal provided by Code Institute for Heroku
 
-1. `heroku/python`
-2. `heroku/nodejs`
+    Steps for deployement
+    - Clone or fork the repository
+    - Install required libraries: `pip install gspread google-auth`
+    - Set up a Google Sheets API and update `credentials.json`
+    - Create a new Heroku App
+    - Set the buildpacks to Python and NodeJS in that order
+    - Link the Heroku App to the repository
+    - Click on DEPLOY
 
-You must then create a _Config Var_ called `PORT`. Set this to `8000`
 
-If you have credentials, such as in the Love Sandwiches project, you must create another _Config Var_ called `CREDS` and paste the JSON into the value field.
+## Usage
 
-Connect your GitHub repository and deploy as normal.
+Run the script and follow the instructions to complete the quiz. The application will guide you through a series of questions to determine your English proficiency level. Results are saved in `results.json`.
 
-## Constraints
 
-The deployment terminal is set to 80 columns by 24 rows. That means that each line of text needs to be 80 characters or less otherwise it will be wrapped onto a second line.
+## Contributing
 
----
+Please fork the repository and submit pull requests.
 
-Happy coding!
+
+## Acknowledgement
+
+    - Reading text: National Geographic Education, Last Updated February 21, 2024, National Geographic Society, National Geographic Society
+    - The template used for building this project was provided by Code Institute on Github for student projects [p3-template](https://github.com/Code-Institute-Org/p3-template)
+    - the Stack Overflow community for pointing me in the right direction when I ran into the scoring issue
+
+
+## Author
+
+- [merzann](https://github.com/merzann)
+
+
+## License
+[![MIT License](https://img.shields.io/badge/License%20-%20MIT-olivgreen)](readme_media/LICENSE.md)
--- run.py
+++ run.py
@@ -7,13 +7,14 @@ import requests
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
     "https://www.googleapis.com/auth/drive.file",
-    "https://www.googleapis.com/auth/drive"
+    "https://www.googleapis.com/auth/drive",
 ]
 
-CREDS = Credentials.from_service_account_file('creds.json')
+CREDS = Credentials.from_service_account_file("creds.json")
 SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
-SHEET = GSPREAD_CLIENT.open('english_level_test')
+SHEET = GSPREAD_CLIENT.open("english_level_test")
+
 
 def sample_question():
     """
@@ -25,12 +26,12 @@ def sample_question():
 
     print("Sample question:\n")
     print("What is the capital of France?\n")
-    
+
     options = ["Berlin", "Madrid", "Paris", "Rome"]
-    
+
     for i, option in enumerate(options, 1):
         print(f"{i}. {option}")
-    
+
     # Validate user input to ensure it's a number within the valid range
     while True:
         try:
@@ -39,12 +40,15 @@ def sample_question():
                 if answer_example == correct_answer:
                     print("You are correct!\n")
                 else:
-                    print(f"Your answer is incorrect. The correct answer is Paris.\n")
+                    print(
+                        f"Your answer is incorrect. Correct answer: Paris.\n"
+                    )
                 break
             else:
                 print("Invalid choice. Please enter a number from 1 to 4.\n")
         except ValueError:
             print("Invalid input. Please enter a number from 1 to 4.\n")
+
 
 def ask_question(question, options, correct_option):
     """
@@ -65,13 +69,15 @@ def ask_question(question, options, correct_option):
         except ValueError:
             print("Invalid input. Please enter a number.")
 
+
 def load_quiz_data(worksheet, num_questions):
     """
     Loads quiz data from the specified worksheet
     """
     questions_sheet = SHEET.worksheet(worksheet)
     questions = questions_sheet.get_all_values()
-    return questions[1:num_questions + 1]
+    return questions[1: num_questions + 1]
+
 
 def calculate_score(questions):
     """
@@ -80,9 +86,9 @@ def calculate_score(questions):
     score = 0
     for question in questions:
         correct = ask_question(
-            question[0], 
-            [question[1], question[2], question[3], question[4]], 
-            question[5]
+            question[0],
+            [question[1], question[2], question[3], question[4]],
+            question[5],
         )
 
         if correct:
@@ -90,43 +96,49 @@ def calculate_score(questions):
         print(f"Current score: {score}")
     return score
 
+
 def comprehension_quiz(sheet):
     """
     Prompts the user with a multiple-choice question
     User request loop to repeat request for data until data provided is valid
     """
-    sheet = SHEET.worksheet('comprehension')
-    
+    sheet = SHEET.worksheet("comprehension")
+
     # Get the reading text from cell A1 and display it
     text = sheet.cell(1, 1).value
     print(text)
 
     score = 0
     questions = sheet.get_all_values()[1:]
-    
+
     for i in range(5):
         question = questions[i]
-        print(f"\n{question[1]}\n")  
-        print(f"1. {question[2]}")  
+        print(f"\n{question[1]}\n")
+        print(f"1. {question[2]}")
         print(f"2. {question[3]}")
         print(f"3. {question[4]}")
         print(f"4. {question[5]}")
-        
+
         while True:
             try:
                 answer = int(input("Your answer (number):\n").strip())
                 if 1 <= answer <= 4:
                     # Convert 'A', 'B', 'C', 'D' to 1, 2, 3, 4 respectively
-                    correct_answer = ord(question[6].strip().upper()) - ord('A') + 1
+                    correct_answer = (
+                        ord(question[6].strip().upper()) - ord("A") + 1
+                    )
                     if answer == correct_answer:
                         score += 1
                     break
                 else:
-                    print("Invalid choice. Please enter a number from the list.")
+                    print(
+                        "Invalid choice. Please enter a number from the list."
+                    )
             except ValueError:
                 print("Invalid input. Please enter a number.")
         print(f"Current score: {score}")
     return score
+
 
 def determine_cefr_level(total_score):
     """
@@ -146,36 +158,61 @@ def determine_cefr_level(total_score):
     else:
         return "C2 - Proficient"
 
-def record_results(name, email, vocab_score, grammar_score, comprehension_score, cefr_level):
+
+def record_results(
+    name, email, vocab_score, grammar_score, comprehension_score, cefr_level
+):
     """
     Records user data + scores + cefr level and appends the data to spreadsheet
     """
     try:
-        results_sheet = SHEET.worksheet('results')
-        results_sheet.append_row([name, email, vocab_score, grammar_score, comprehension_score, cefr_level])
-        print("Results recorded successfully. An email will be sent to you shortly.")
+        results_sheet = SHEET.worksheet("results")
+        results_sheet.append_row(
+            [
+                name,
+                email,
+                vocab_score,
+                grammar_score,
+                comprehension_score,
+                cefr_level,
+            ]
+        )
+        print("Results recorded successfully. Results will be sent shortly.")
+
     except Exception as e:
         print(f"Apologies, an error occurred while recording the results: {e}")
+
 
 def send_to_zapier_webhook(results):
     """
     Save results to Zapier for sending results to user via email
     """
     try:
-        with open('zapier_webhook_url.txt', 'r') as file:
+        with open("zapier_webhook_url.txt", "r") as file:
             zapier_webhook_url = file.read().strip()
-        
+
         response = requests.post(zapier_webhook_url, json=results)
         if response.status_code == 200:
             print("Successfully sent results to Z.")
         else:
-            print(f"Failed to send results to Z. Status code: {response.status_code}")
+            print(
+                f"Failed to send results. Status code: {response.status_code}"
+            )
     except Exception as e:
         print(f"Error sending data to Z.: {e}")
 
-def save_results(filename, name, email, vocab_score, grammar_score, comprehension_score, cefr_level):
+
+def save_results(
+    filename,
+    name,
+    email,
+    vocab_score,
+    grammar_score,
+    comprehension_score,
+    cefr_level,
+):
     """
-    Saves results to results.json to be extracted upon sending results email to user
+    Saves results to results.json to be extracted upon sending results to user
     """
     results = {
         "name": name,
@@ -183,11 +220,11 @@ def save_results(filename, name, email, vocab_score, grammar_score, comprehensio
         "vocabulary_score": vocab_score,
         "grammar_score": grammar_score,
         "text_comprehension_score": comprehension_score,
-        "cefr_level": cefr_level
+        "cefr_level": cefr_level,
     }
     # check that results are saved correctly to results.json
     try:
-        with open(filename, 'w') as file:
+        with open(filename, "w") as file:
             json.dump(results, file)
         print(f"Results saved to {filename}")
 
@@ -197,6 +234,7 @@ def save_results(filename, name, email, vocab_score, grammar_score, comprehensio
     except IOError as e:
         print(f"An error occurred while saving results to file: {e}")
 
+
 def main():
     """
     Run all program functions
@@ -205,13 +243,13 @@ def main():
 
     print("Loading English Language test ...\n")
 
-    vocab_questions = load_quiz_data('vocabulary', 15)
+    vocab_questions = load_quiz_data("vocabulary", 15)
     vocab_score = calculate_score(vocab_questions)
-    
-    grammar_questions = load_quiz_data('grammar', 15)
+
+    grammar_questions = load_quiz_data("grammar", 15)
     grammar_score = calculate_score(grammar_questions)
-    
-    comprehension_score = comprehension_quiz(SHEET.worksheet('comprehension'))
+
+    comprehension_score = comprehension_quiz(SHEET.worksheet("comprehension"))
 
     total_score = vocab_score + grammar_score + comprehension_score
     cefr_level = determine_cefr_level(total_score)
@@ -222,15 +260,32 @@ def main():
     print(f"Text Comprehension Section Score: {comprehension_score}/5")
     print(f"Your CEFR Level is: {cefr_level}")
 
-    record_results(name, email, vocab_score, grammar_score, comprehension_score, cefr_level)
-    save_results("results.json", name, email, vocab_score, grammar_score, comprehension_score, cefr_level)
+    record_results(
+        name,
+        email,
+        vocab_score,
+        grammar_score,
+        comprehension_score,
+        cefr_level,
+    )
+    save_results(
+        "results.json",
+        name,
+        email,
+        vocab_score,
+        grammar_score,
+        comprehension_score,
+        cefr_level,
+    )
+
 
 print("Welcome to the learning platform of Anne's Language Retreat\n")
 print("Discover your level of English with our free online test.")
 print("The test takes 10 - 15min to complete.")
 print("Input the number (1 - 4) of the correct answer and press enter\n")
 
-print("Please enter your first and lastname and your email address.\nA copy of your result will be send to your email after completing the test.\n")
+print("Please enter your first and lastname and your email address.")
+print("Your result will be send to your email after completing the test.\n")
 
 while True:
     name = input("Please enter your name:\n").strip()
@@ -243,7 +298,9 @@ while True:
     try:
         email = input("Please enter your email:\n").strip()
         if not re.match(r"[^@]+@[^@]+\.[^@]+", email):
-            raise ValueError("Invalid email format. The format should be name@email.com")
+            raise ValueError(
+                "Invalid email format. The format should be name@email.com"
+            )
     except ValueError as e:
         print(e)
     else:
